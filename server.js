f6c85597b00618fae9f30b9ca5edf1f1b09eb539
pink_glb <adriancosta1215@gmail.com>
2022-10-20 17:37:29 -0400

    aws deploy configuration

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ server.use(express.json());
 server.use(sendemail);
 
 server.listen(process.env.PORT, ()=>{
-    console.log("Server running on port " + process.env.PORT)
+    console.log("Server running on port " + 5000)
 });
--- src/config/mailer.js
+++ src/config/mailer.js
@@ -8,7 +8,7 @@ export const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-      user: process.env.EMAIL_USER, 
-      pass: process.env.EMAIL_PASS,
+      user: "adriancosta1215@gmail.com", 
+      pass: "izwpxaomrjuwanpe",
     },
 });

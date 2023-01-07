395090c5831e30443cda955c81b1b2f70860e4aa
pink_glb <adriancosta1215@gmail.com>
2023-01-07 15:45:56 -0400

    mongodb cluster address added

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,6 +1,6 @@
 import { MongoClient, ObjectId } from "mongodb";
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient("mongodb+srv://dev:u268fDPETCPLX9zF@cluster0.gar4r1r.mongodb.net/?retryWrites=true&w=majority");
 let db;
 
 mongoClient.connect().then(() => {
